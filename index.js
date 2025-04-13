6798117135c3c26617b0ae68edff204a0fa53019
rakaputar <ikhramraka@gmail.com>
2025-04-13 21:51:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.use(cors());
 app.use(express.json());
 app.use(route);
 
-
+app.get('/', (req, res) => {
+  res.send('running...');
+});
 
 app.listen(3000,()=> console.log("server berjalan di port 3000"))
